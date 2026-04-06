7e3f0e1f8ea83e99e79685c2a2e7f2dbec5935c6
sergiorupay-bcpconsulting <srupay@bcpconsulting.com>
2026-04-06 11:14:22 -0500

    first commit

---
+++ src/main/java/com/utp/backwebintegrado/BackWebIntegradoApplication.java
@@ -0,0 +1,13 @@
+package com.utp.backwebintegrado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackWebIntegradoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackWebIntegradoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/utp/backwebintegrado/BackWebIntegradoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utp.backwebintegrado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackWebIntegradoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
