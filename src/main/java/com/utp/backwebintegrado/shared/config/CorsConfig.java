75ba43e8e68b36c57fb5d26ad43363d952b1d8c6
Andrer50 <sergiorupay990@gmail.com>
2026-05-11 14:37:01 -0500

    feat: implement CORS, security configuration, and dev environment settings

--- src/main/java/com/utp/backwebintegrado/shared/config/CorsConfig.java
+++ src/main/java/com/utp/backwebintegrado/shared/config/CorsConfig.java
@@ -5,11 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
+import org.springframework.context.annotation.Configuration;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+@Configuration
 public class CorsConfig {
     @Value("${spring.uri-front}")
     private String uriFront;
@@ -17,7 +18,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList(uriFront));
+        configuration.setAllowedOriginPatterns(Collections.singletonList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH" ,"DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
